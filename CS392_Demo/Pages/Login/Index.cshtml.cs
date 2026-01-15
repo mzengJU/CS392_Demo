484404568b5c0465199e2d99bd8abf8f04f8e8b8
Mini Zeng <beckymini.mz0005@hotmail.com>
2026-01-15 10:48:47 -0500

    test featur/001_login

---
+++ CS392_Demo/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS392_Demo.Pages.Login
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
